fec51fc41d91520f6fad0e1414bc7647a535aa05
Martin Lidgren <martin.lidgren@live.se>
2023-09-20 12:30:08 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        Pokemon mördarPoke = new Pokemon("Bulbasaur", 200, PokemonType.GRASS);
+        Pokemon attacker = new Pokemon("Bulbasaur", 200, PokemonType.GRASS);
         Pokemon defender = new Pokemon("Squirtle", 200, PokemonType.NORMAL);
-        Skill gräsSKill = new Skill("mördarLöv", 101, 40);
-        mördarPoke.learnSkill(gräsSKill);
-        mördarPoke.useSkill(defender);
-        mördarPoke.useSkill(defender);
-        mördarPoke.useSkill(defender);
+        Skill leafSlash = new Skill("Leaf Slash", 101, 40);
+        attacker.learnSkill(leafSlash);
+        attacker.useSkill(defender);
+        attacker.useSkill(defender);
+        attacker.useSkill(defender);
     }
 }
