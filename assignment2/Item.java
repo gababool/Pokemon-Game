47bd8201ef335fe4dfc7de04799c9e03bf8d1504
Emmaolmas <emma.olmas@gmail.com>
2023-09-21 14:36:38 +0200

    Update Item.java

--- assignment2/Item.java
+++ assignment2/Item.java
@@ -1,6 +1,29 @@
 package assignment2;
 
 public class Item {
+    private final String name;
+    private final int powerValue;
+    private final double weight;
+
+    public Item(String name, int powerValue, double weight){
+
+        this.name = name;
+        this.powerValue = powerValue;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPowerValue() {
+        return powerValue;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
 
     
+
 }
