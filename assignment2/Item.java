13371b15f943b935e8b00e588ace2b6b2ce65120
Emmaolmas <emma.olmas@gmail.com>
2023-09-21 20:33:19 +0200

    removed dead code

--- assignment2/Item.java
+++ assignment2/Item.java
@@ -1,8 +1,5 @@
 package assignment2;
 
-//import java.math.RoundingMode;
-//import java.text.DecimalFormat;
-
 public class Item {
     private final String name;
     private final int powerValue;
@@ -30,19 +27,6 @@ public class Item {
         double factor = Math.pow(10, 2);
         return Math.floor(value * factor) / factor;
     }
-
-
-    //private String truncateDouble3(double value){
-      //  DecimalFormat df = new DecimalFormat("#.##");
-        //df.setRoundingMode(RoundingMode.DOWN);
-        //return df.format(value);
-    //}
-
-
-   // private double truncateDouble2(double value){
-     //   double truncated = ((int)(value * 100)) / 100;
-       // return truncated;
-    //}
 
     @Override
     public String toString(){
