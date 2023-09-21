0c58e50df2b1cf25f274f55c2aa25d7057255529
lovecarstr <love.strandang@gmail.com>
2023-09-21 10:57:26 +0200

    lawdliajwdlijawlidj
    lalalalla

--- assignment2/TypeEffectiveness.java
+++ assignment2/TypeEffectiveness.java
@@ -1,9 +1,10 @@
 package assignment2;
+
 public class TypeEffectiveness {
 
     public static double calculateMultiplier(String attacker, String defender) {
         double multiplier = 1.0;
-        
+
         switch (attacker) {
             case "Fire":
                 switch (defender) {
@@ -11,10 +12,10 @@ public class TypeEffectiveness {
                         multiplier = 0.5;
                         break;
                     case "Grass":
-                        multiplier= 2.0;
+                        multiplier = 2.0;
                         break;
                     case "Water":
-                        multiplier= 0.5;
+                        multiplier = 0.5;
                         break;
                     default:
                         break;
@@ -22,50 +23,48 @@ public class TypeEffectiveness {
                 break;
             case "Water":
                 switch (defender) {
-                        case "Fire":
-                            multiplier= 2.0;
-                            break;
-                        case "Grass":
-                            multiplier= 0.5;
-                            break;
-                        case "Water":
-                            multiplier= 0.5;
-                            break;
-                        default:
-                            break;
+                    case "Fire":
+                        multiplier = 2.0;
+                        break;
+                    case "Grass":
+                        multiplier = 0.5;
+                        break;
+                    case "Water":
+                        multiplier = 0.5;
+                        break;
+                    default:
+                        break;
                 }
                 break;
             case "Grass":
                 switch (defender) {
-                        case "Fire":
-                            multiplier= 0.5;
-                            break;
-                        case "Grass":
-                            multiplier= 0.5;
-                            break;
-                        case "Water":
-                            multiplier= 2.0;
-                            break;
-                        default:
-                            break;
-                        
+                    case "Fire":
+                        multiplier = 0.5;
+                        break;
+                    case "Grass":
+                        multiplier = 0.5;
+                        break;
+                    case "Water":
+                        multiplier = 2.0;
+                        break;
+                    default:
+                        break;
+
                 }
                 break;
             case "Normal":
                 break;
-            
-        
+
         }
         return multiplier;
     }
 
-    public static String generateEffectivenessMsg(double multiplier){
-        if (multiplier == 0.5){
-            return "It is not very effective...";
-        } else if (multiplier == 2.0){
-            return "It is super effective!";
-        }
-        else{
+    public static String generateEffectivenessMsg(double multiplier) {
+        if (multiplier == 0.5) {
+            return " It is not very effective...";
+        } else if (multiplier == 2.0) {
+            return " It is super effective!";
+        } else {
             return "";
         }
     }
