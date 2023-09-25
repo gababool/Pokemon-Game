66c3b63263824a68e8221b48c1b37ee884b6f7f5
Martin Lidgren <martin.lidgren@live.se>
2023-09-25 13:17:47 +0200

    Update ItemBag.java

--- assignment2/ItemBag.java
+++ assignment2/ItemBag.java
@@ -1,15 +1,19 @@
 package assignment2;
+
+import java.util.ArrayList;
+
 public class ItemBag {
 
     private final double MAX_WEIGHT;
     private double currentWeight;
-    private Item[] storedItems;
+    private ArrayList<Item> storedItems;
+    private int numOfItems;
     
     public ItemBag(double MAX_WEIGHT){
         this.MAX_WEIGHT = MAX_WEIGHT;
         this.currentWeight = 0;
-        this.storedItems = new Item[Integer.MAX_VALUE];
-
+        this.storedItems = new ArrayList<>();
+        this.numOfItems = 0;
     }
 
     public int addItem(Item item){
@@ -17,9 +21,18 @@ public class ItemBag {
         if (weight + currentWeight > MAX_WEIGHT){
             return -1;
         } else {
+            storedItems.add(item);
             int itemIndex = sortItems(weight);
             return itemIndex;
         }
+    }
+
+    public Item removeItemAt(int index){
+        return item;
+    }
+
+    public Item popItem(){
+        return item;
     }
 
     public int sortItems(double weight){
@@ -37,19 +50,32 @@ public class ItemBag {
 
         while (weightCounter <= MAX_WEIGHT){
             for (int i = 0; i < storedItems.length; i++){
-                weightCounter += storedItems[i].getWeight();
-                
-        }
-        
-            
-         
-        }
-      
+                weightCounter += storedItems[i].getWeight();    
+            }
+        } 
     }
 
     public void placeItem(){
         // When item is added to bag, sout index position
     }
+
+    public Item peekItemAt(int index){
+        return item;
+    }
+
+    public double getMAX_WEIGHT() {
+        return MAX_WEIGHT;
+    }
+
+    public double getCurrentWeight() {
+        return currentWeight;
+    }
+
+    public int getNumOfItems() {
+        return numOfItems;
+    }
+
+    
 
 
 }
